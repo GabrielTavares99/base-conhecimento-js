5bea44381fe478a735ebe0fba27d4965ceb013e5
Gabriel Tavares <gabriel.silva@onebox.net.br>
2018-02-07 14:49:54 -0200

    Query selector do elemento pai - calculo de IMC

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,20 @@
 //	var titulo = document.querySelector("h1");
-var titulo = document.querySelector(".titulo");
+let titulo = document.querySelector(".titulo");
 titulo.textContent = "you was hacked";
+
+let paciente = document.querySelector("#paciente1");
+
+let peso = paciente.querySelector(".info-peso").textContent;
+let altura = paciente.querySelector(".info-altura").textContent;
+let imc = paciente.querySelector(".info-imc");
+
+let resultadoImc = peso / (altura*altura);
+
+if (peso <= 0 || peso > 500)
+    resultadoImc = "Peso inválido";
+
+if (altura <= 0 || altura > 3)
+    resultadoImc = "Altura inválida";
+
+
+imc.textContent = resultadoImc;
